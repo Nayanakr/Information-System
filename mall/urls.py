eac263c42236945a3e3aebc201bbe5b2dc83fe18
Nayanakr <nayanakrish768@gmail.com>
2025-04-10 11:42:04 +0530

    customer list updated

--- mall/urls.py
+++ mall/urls.py
@@ -6,5 +6,19 @@ urlpatterns = [
 
     # Customer URLs
     path('customers/', views.customer_list, name='customer_list'),
-   
+    path('customers/add/', views.add_customer, name='add_customer'),
+    path('customers/edit/<int:id>/', views.edit_customer, name='edit_customer'),
+    path('customers/delete/<int:customer_id>/', views.delete_customer, name='delete_customer'),
+
+   # Employee URLs
+    path('employees/', views.employee_list, name='employee_list'),
+    path('employees/add/', views.add_employee, name='add_employee'),
+    path('employees/edit/<int:id>/', views.edit_employee, name='edit_employee'),
+    path('employees/delete/<int:id>/', views.delete_employee, name='delete_employee'),
+
+     # Shop URLs
+    path('shops/', views.shop_list, name='shop_list'),
+    path('shops/add/', views.add_shop, name='add_shop'),
+    path('shops/edit/<int:id>/', views.edit_shop, name='edit_shop'),
+    path('shops/delete/<int:id>/', views.delete_shop, name='delete_shop'),
 ]
