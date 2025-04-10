46b30f9f6e0f1af86b16d50ba73f5cba05fdd431
Nayanakr <nayanakrish768@gmail.com>
2025-04-10 11:41:12 +0530

    customer list updated

--- mall/models.py
+++ mall/models.py
@@ -1,31 +1,3 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=15)
-    purchases = models.TextField()
-    feedback = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-class Employee(models.Model):
-    name = models.CharField(max_length=255)
-    position = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-class Shop(models.Model):
-    name = models.CharField(max_length=255)
-    owner = models.CharField(max_length=255)
-    category = models.CharField(max_length=100)
-    contact = models.CharField(max_length=15)
-    rent = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
