c21734db33f422b96b51f5384f79a202e17bef9f
Nayanakr <nayanakrish768@gmail.com>
2025-04-10 11:52:32 +0530

    model updated

--- mall/models.py
+++ mall/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    purchases = models.TextField()
+    feedback = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Employee(models.Model):
     name = models.CharField(max_length=255)
