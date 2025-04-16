b5274b99006b96ff7d5e22e38076405fab1c21ef
Nayanakr <115971729+Nayanakr@users.noreply.github.com>
2025-04-17 01:08:37 +0530

    update changes

--- mall/models.py
+++ mall/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Customer(model.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
     purchases = models.TextField()
